6ebbdadd9b73d9167824114d2b64e61003ddbdca
BjornDCode <bjorn@lindholmhansen.com>
2017-07-22 14:28:23 +0200

    Basic setup

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
+    public function home() {
+        return view('public.home');
+    }
+
+}
---
+++ resources/views/public/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.static')
+
+@section('content')
+    <main class="container">
+        <h1>Timian</h1>
+        <div class="button-group">
+            <a href="#">Sign Up</a>
+            <a href="#">Sign In</a>
+        </div>
+    </main>
+@endsection
